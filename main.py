1dc467290366f187ca2e8992a8571f01e1af5da4
Sebastian <119985724+developer-seb-v@users.noreply.github.com>
2024-01-12 13:46:37 -0800

    tkinter input working now with sqlite3

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
 # tkinter gui app
 import tkinter
 from tkinter import *
+
 from tkcalendar import DateEntry
-from db import *
+
+from query import *
 
 # tkinter
+
+# db
+# create db
+conn = sqlite3.connect('database.db')
+
+# create cursor
+c = conn.cursor()
+
+# # create table
+# c.execute(
+#    """
+#    CREATE TABLE IF NOT EXISTS goal (
+#    id INTEGER PRIMARY KEY AUTOINCREMENT,
+#    name VARCHAR(255) NOT NULL,
+#    description VARCHAR(255) NOT NULL,
+#    date_created DATETIME NOT NULL
+#    )
+#    """
+# )
+
+
+# commit changes
+conn.commit()
+
+# close connection
+conn.close()
+
+
+# queries
+def insert(name, description, date_created):
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute("""
+       INSERT INTO goal (name, description, date_created)
+       VALUES(?,?,?)
+      """, (name, description, date_created))
+    conn.commit()
+    conn.close()
+
 
 # window properties
 main = Tk()
@@ -36,12 +77,10 @@ due_date_label.pack(side=TOP)
 # due date picker widget
 date_picker = DateEntry(main, width=10)
 date_picker.pack(side=TOP)
-date_picker_input = date_picker.get_date().strftime("%m/%d/%Y")
-
+date_picker_input = date_picker.get_date()
 
 # button with sql insert method as command
-add_button = Button(main, text="Add Goal")
+add_button = Button(main, text="Add Goal", command=lambda:  insert("lose weight", "lose 10 lbs", "06-01-2024"))
 add_button.pack(side=TOP)
-
 
 main.mainloop()

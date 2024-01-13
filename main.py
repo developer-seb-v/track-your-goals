ca994232e9e0591a67df862f617a633d750f9933
Sebastian <119985724+developer-seb-v@users.noreply.github.com>
2024-01-13 14:08:51 -0800

    tkinter input working now with sqlite3 (actually working). Had to use StringVar(), then use get() method for sqlite3 to recognize the string being passed in the insert query.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # tkinter gui app
+import datetime
 import tkinter
 from tkinter import *
 
@@ -15,17 +16,17 @@ conn = sqlite3.connect('database.db')
 # create cursor
 c = conn.cursor()
 
-# # create table
-# c.execute(
-#    """
-#    CREATE TABLE IF NOT EXISTS goal (
-#    id INTEGER PRIMARY KEY AUTOINCREMENT,
-#    name VARCHAR(255) NOT NULL,
-#    description VARCHAR(255) NOT NULL,
-#    date_created DATETIME NOT NULL
-#    )
-#    """
-# )
+# create table
+c.execute(
+   """
+   CREATE TABLE IF NOT EXISTS goal (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   name VARCHAR(255) NOT NULL,
+   description VARCHAR(255) NOT NULL,
+   date_created DATETIME NOT NULL
+   )
+   """
+)
 
 
 # commit changes
@@ -36,13 +37,13 @@ conn.close()
 
 
 # queries
-def insert(name, description, date_created):
+def insert(n, descr, created: datetime):
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
     c.execute("""
        INSERT INTO goal (name, description, date_created)
        VALUES(?,?,?)
-      """, (name, description, date_created))
+      """, (n, descr, created))
     conn.commit()
     conn.close()
 
@@ -58,29 +59,31 @@ top_frame.pack(side=TOP, fill='both')
 label = tkinter.Label(text="Goal Name", font="Arial")
 label.pack(side=TOP)
 
+name = StringVar()
+desc = StringVar()
+
+
 # goal name entry widget
-goal_name = Entry(main, width=30)
+goal_name = Entry(main, textvariable=name, width=30)
 goal_name.pack(side=TOP)
-goal_name_input = goal_name.get()
 
 desc_label = tkinter.Label(text="Goal Description", font="Arial")
 desc_label.pack(side=TOP)
 
 # goal description entry widget
-goal_description = Entry(main, width=30)
+goal_description = Entry(main, textvariable=desc, width=30)
 goal_description.pack(side=TOP)
-goal_description_input = goal_description.get()
 
 due_date_label = tkinter.Label(text="Due Date", font="Arial")
 due_date_label.pack(side=TOP)
 
 # due date picker widget
-date_picker = DateEntry(main, width=10)
+date_picker = DateEntry(main,  width=10)
 date_picker.pack(side=TOP)
-date_picker_input = date_picker.get_date()
+date_picker_input = date_picker.get()
 
 # button with sql insert method as command
-add_button = Button(main, text="Add Goal", command=lambda:  insert("lose weight", "lose 10 lbs", "06-01-2024"))
+add_button = Button(main, text="Add Goal", command=lambda:  insert(name.get(), desc.get(), date_picker_input))
 add_button.pack(side=TOP)
 
 main.mainloop()
